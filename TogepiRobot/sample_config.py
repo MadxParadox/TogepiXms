171c4601f934449b76e1f5ecdbb25640441adfc1
MadxParadox <95468343+MadxParadox@users.noreply.github.com>
2021-12-08 14:25:33 +0530

    Update sample_config.py

--- TogepiRobot/sample_config.py
+++ TogepiRobot/sample_config.py
@@ -15,14 +15,14 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 2091894  # integer value, dont use ""
-    API_HASH = "925ce96bd585205d6dac1b35e8e26a12"
-    TOKEN = "1616584987:AAGiKBL-7Q4mihWH03AGtyBjfDNdp23zJVc"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 769830161 # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Rimbahuns"
-    SUPPORT_CHAT = 'allbefin'  #Your own group for support, do not add the @
-    JOIN_LOGGER = -1360279453  #Prints any new group the bot is added to, prints just the name and ID.
-    EVENT_LOGS = -1360279453  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+    API_ID = 3621722  # integer value, dont use ""
+    API_HASH = "1424ea0f4be038f55137774f9ba81c3d"
+    TOKEN = "5016661698:AAGmMwuvPPQfOGnwak88K74HKOfxYJuj870"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 2068508072 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Btw_Its_Paradox"
+    SUPPORT_CHAT = 'RhythmXChill'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -1001578626244  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -1001578626244  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
